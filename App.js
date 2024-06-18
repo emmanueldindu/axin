b4521eb67c7ad1d135cc2068de724b8f4a91352c
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-06-18 10:17:26 +0100

    tusday

--- App.js
+++ App.js
@@ -2,19 +2,26 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Layout from "./app/Layout";
 
+import Layout from "./app/Layout";
+import Home from "./app/(tabs)/Home";
+import ButtonTab from "./app/navigation/ButtonTab";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+
         <Stack.Screen
           name="index"
           component={Layout}
           options={{ headerShown: false }}
         ></Stack.Screen>
+        <Stack.Screen name="ButtonTab" component={ButtonTab}></Stack.Screen>
+
+
+        <Stack.Screen name="Home" component={Home}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- app/Layout.jsx
+++ app/Layout.jsx
@@ -1,25 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useFonts } from 'expo-font';
+import { StyleSheet, ScrollView, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+import { useFonts } from "expo-font";
+import { Link } from "expo-router";
+import { useNavigation } from "@react-navigation/native";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import { ScrollView } from "react-native-gesture-handler";
 
 const Layout = () => {
+  const navigation = useNavigation();
+
+  return (
+  <SafeAreaView className='bg-primary h-full'>
+    <ScrollView contentContainerStyle={{height: '100%'}}>
+      <View className='w-full justify-center item-center h-full px-4'>
+        
+
+      </View>
+
+    </ScrollView>
 
 
-    return (
-        <View className=' mt-12'>
-          <Text className='text-black font-semibold'>Axin</Text>
-          {/* <StatusBar style="auto" /> */}
-        </View>
-      );
-    }
-    
-    export default Layout
-    
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-    });
+
+  </SafeAreaView>
+  );
+};
+
+export default Layout;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
