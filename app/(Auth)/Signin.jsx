965751b378bf737a273367b596710e5a7069f904
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-06-20 10:45:45 +0100

    thursday

--- app/(Auth)/Signin.jsx
+++ app/(Auth)/Signin.jsx
@@ -1,16 +1,45 @@
-import { View, Text, ScrollView } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import FormFied from "../../components/FormFied";
 
 const Signin = () => {
+  cont[(form, setForm)] = useState({
+    email: "",
+    password: "",
+  });
   return (
-  <SafeAreaView className='bg-primary h-full'>
-    <ScrollView className='w-full  min-h-[85vh]'> 
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{ justifyContent: "center" }}
+        className="w-full   px-4 my-6  min-h-[85vh]"
+      >
+        <Text className="text-3xl  text-white font-semibold">
+          Ax<Text className="">Vid</Text>
+        </Text>
 
-    </ScrollView>
+        <Text className="text-white text-2xl font-semibold mt-10 ">
+          Log in to Ax<Text className="text-[#FFB500]">Vid</Text>
+        </Text>
 
-  </SafeAreaView>
-  )
-}
+        <FormFied
+          title="Email"
+          value={form.email}
+          handleChangeText={(e) => setForm({ ...form, email: e })}
+          otherStyles="mt-7"
+          keyboardType="email-address"
+        />
 
-export default Signin
+        <FormFied
+          title="Email"
+          value={form.email}
+          handleChangeText={(e) => setForm({ ...form, email: e })}
+          otherStyles="mt-7"
+          keyboardType="email-address"
+        />
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Signin;
---
+++ components/FormFied.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FormFied = () => {
+  return (
+    <View className='space-y-2'>
+
+      <Text className='text-base text-gray-100 '>FormFied</Text>
+    </View>
+  )
+}
+
+export default FormFied
