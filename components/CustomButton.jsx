2a1abe18982661f878dd529fedb11ce8859c7aab
emmanueldindu <emmanuelnwobodoc04@gmail.com>
2024-06-21 23:16:15 +0100

    custom button

--- components/CustomButton.jsx
+++ components/CustomButton.jsx
@@ -1,9 +1,14 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const CustomButton = () => {
+const CustomButton = ({title, handlePress, continerStyles, textStyles, isLoading }) => {
   return (
-<TouchableOpacity className='bg-white rounded-xl justify-center items-center'>
+<TouchableOpacity
+className={`bg-secondary rounded-xl justify-center items-center ${continerStyles} ${isLoading ? 'opacity-50' : ''}`}
+onPress={handlePress}
+disabled={isLoading}
+activeOpacity={0.7}
+>
 
   <Text className='text-primary font-semibold text-lg'>CustomButton</Text>
 </TouchableOpacity>
--- components/FormFied.jsx
+++ components/FormFied.jsx
@@ -1,6 +1,7 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
 import React, {useState} from "react";
-
+import images from "../constants/images";
+import icons from '../constants/icons'
 const FormFied = ({
   title,
   value,
@@ -14,7 +15,7 @@ const FormFied = ({
   return (
     <View className={`space-y-2 ${otherStyles}`}>
       <Text className="text-gray-300">{title}</Text>
-      <View className="w-full h-16 bg-black-100 rounded-xl px-6 focus:border-secondary items-center">
+      <View className="w-full h-16 bg-black-100 rounded-xl px-6 focus:border-secondary items-center flex-row">
         <TextInput
         className='flex-1 h-full w-full text-white font-semibold'
         value={value}
@@ -23,6 +24,14 @@ const FormFied = ({
         placeholderTextColor='#7b78b'
         secureTextEntry={title === 'Password' && !showPassword}
         />
+
+        {title === 'Password' && (
+            <TouchableOpacity 
+            onPress={() => setShowPassword(!showPassword) }>
+            <Image source={!showPassword ? icons.eye : icons.eyeHide } className='w-6 h-6' resizeMode="contain"  />
+            
+            </TouchableOpacity>
+        )}
       </View>
     </View>
   );
